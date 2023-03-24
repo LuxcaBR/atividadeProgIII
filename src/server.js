9acc69e733fb46201ca5de9706c79010a7ebdb38
Lucas <luxcafaleiro@gmail.com>
2023-03-24 20:28:01 -0300

    kkk

---
+++ src/server.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = (0, express_1["default"])();
+var port = 3000;
+app.get('/', function (request, response) {
+    response.send('Hello World! - Lucas, FEMA');
+});
+app.listen(port, function () {
+    console.log("Server Running! <3 ");
+});
