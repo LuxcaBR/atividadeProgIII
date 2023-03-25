971e769f98c571839abef02e12dd1ebdafc78e2f
Lucas <luxcafaleiro@gmail.com>
2023-03-25 14:55:19 -0300

    testt

--- src/router/route.user.ts
+++ src/router/route.user.ts
@@ -73,4 +73,83 @@ userRoute.put("/:id", (request, response) => {
   response.status(201).json({ msg: `O ID: {${id}} foi alterado banco` });
 });
 
+
+const router = Router();
+
+let users = [
+  {
+    id: 1,
+    nome: 'usuario1',
+    email: 'usuario1@gmail.com',
+    valor: 100.0
+  },
+  {
+    id: 2,
+    nome: 'usuario2',
+    email: 'usuario2@gmail.com',
+    valor: 200.0
+  }
+];
+
+router.get('/users', (req, res) => {
+  res.json(users);
+});
+
+userRoute.get('/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não encontrado!' });
+  }
+  res.json(user);
+});
+
+userRoute.post('/users', (req, res) => {
+  const { nome, email, valor } = req.body;
+  const id = users.length + 1;
+  const user = { id, nome, email, valor };
+  users.push(user);
+  res.json(user);
+});
+
+userRoute.put('/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não encontrado!' });
+  }
+  const { nome, email, valor } = req.body;
+  user.nome = nome;
+  user.email = email;
+  user.valor = valor;
+  res.json(user);
+});
+
+userRoute.delete('/users/:id', (req, res) => {
+  users = users.filter(u => u.id !== parseInt(req.params.id));
+  res.json({ message: 'User deleted' });
+});
+
+userRoute.post('/users/:id/deposit', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não encontrado!' });
+  }
+  const { amount } = req.body;
+  user.valor += amount;
+  res.json(user);
+});
+
+userRoute.post('/users/:id/withdraw', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não encontrado!' });
+  }
+  const { amount } = req.body;
+  if (user.valor < amount) {
+    return res.status(400).json({ message: 'Valor insuficiente' });
+  }
+  user.valor -= amount;
+  res.json(user);
+});
+
 export { userRoute };
+
